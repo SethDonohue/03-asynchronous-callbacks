54c54dbc27853a9dd485d14347c740f35edef792
SethDonohue <donohue.seth@gmail.com>
2017-11-29 17:26:24 -0800

    GREEN tests, bad filepath tested

--- lab-seth/__test__/reader.test.js
+++ lab-seth/__test__/reader.test.js
@@ -4,13 +4,22 @@ const reader = require('../lib/reader.js');
 
 const paths = [`${__dirname}/../assets/data1.txt`, `${__dirname}/../assets/data2.txt`, `${__dirname}/../assets/data3.txt`];
 
+const badPaths = [`${__dirname}/../sets/data1.txt`, `${__dirname}/assets/data1.txt`, `${__dirname}/../failure/data1.txt`];
+
+const dataCheck = ['birds\n','animals\n','spiders\n'];
+
 describe('reader.readAll', () => {
 
-  test('fs.readFile should return a callback with the result being the data of the file read', (done) => {
+  test('reader.readAll should return a callback with the result being the data of the file read', (done) => {
     reader.readAll(paths, (err, data) => {
       expect(err).toBeNull();
-      expect(data).toEqual(`animals cats dogs dinosaurs
-`);
+      expect(data).toEqual(dataCheck);
+      done();
+    });
+  });
+  test('reader.ReadAll should return an error when given an invlaid filepath', (done) => {
+    reader.readAll(badPaths, (err) => {
+      expect(err).not.toBeNull();
       done();
     });
   });
--- lab-seth/lib/reader.js
+++ lab-seth/lib/reader.js
@@ -7,12 +7,20 @@ const fileInfo = [];
 
 //can only handle a certain number of files determined by how many readFile functions there are inside of readAll.
 reader.readAll = (paths, callback) => {
-  //write error checks here for all readFile functions
-  console.log(paths[0]);
 
   fs.readFile(paths[0], 'utf8', (err, data) => {
     if (err) return callback(err);
-    fileInfo.push(data.toString('utf-8', 0));
-    return callback(null, fileInfo[0]);
+    fileInfo.push(data.toString('utf-8'));
+
+    fs.readFile(paths[1], 'utf8', (err, data) => {
+      if (err) return callback(err);
+      fileInfo.push(data.toString('utf-8'));
+
+      fs.readFile(paths[2], 'utf8', (err, data) => {
+        if (err) return callback(err);
+        fileInfo.push(data.toString());
+        callback(null, fileInfo);
+      });
+    });
   });
 };
